0d8d321d5050887eed03551199ef09cf4bdb6d88
Vojtech Rinik <vojto@rinik.net>
2017-04-25 13:10:11 +0200

    Adding functions from MedianLayout

--- NiceKit/NSView+Layout.swift
+++ NiceKit/NSView+Layout.swift
@@ -115,3 +115,209 @@ extension XView {
         }
     }
 }
+
+
+
+public extension NSView {
+    func removeSubviews() {
+        for subview in subviews {
+            subview.removeFromSuperview()
+        }
+    }
+}
+
+public enum StackDirection {
+    case column
+    case row
+}
+
+public extension LayoutProxy {
+    public func start(_ direction: StackDirection) -> Edge {
+        switch direction {
+        case .column:
+            return left
+        case .row:
+            return top
+        }
+    }
+    
+    public func end(_ direction: StackDirection) -> Edge {
+        switch direction {
+        case .column:
+            return right
+        case .row:
+            return bottom
+        }
+    }
+    
+    public func edge(type: EdgeType) -> Edge {
+        switch type {
+        case .top:
+            return top
+        case .bottom:
+            return bottom
+        case .left:
+            return left
+        case .right:
+            return right
+        case .centerX:
+            return centerX
+        case .centerY:
+            return centerY
+        }
+    }
+}
+
+public func alignEdges(_ direction: StackDirection, _ align: StackAlign) -> [EdgeType] {
+    switch direction {
+    case .column:
+        switch align {
+        case .start:
+            return [.top]
+        case .center:
+            return [.centerY]
+        case .end:
+            return [.bottom]
+        case .stretch:
+            return [.top, .bottom]
+        }
+    case .row:
+        switch align {
+        case .start:
+            return [.left]
+        case .center:
+            return [.centerX]
+        case .end:
+            return [.right]
+        case .stretch:
+            return [.left, .right]
+        }
+    }
+}
+
+
+
+public enum EdgeType {
+    case top
+    case centerY
+    case centerX
+    case bottom
+    case left
+    case right
+}
+
+public enum StackAlign {
+    case start
+    case center
+    case end
+    case stretch
+}
+
+public extension Array where Element:NSView {
+    public func stack(_ dir: StackDirection, margin: CGFloat = 0, align: StackAlign = .center) -> NSView {
+        let container = NSView()
+        
+        for view in self {
+            container.addSubview(view)
+        }
+        
+        // Align all children
+        for view in self {
+            constrain(view) { view in
+                for edgeType in alignEdges(dir, align) {
+                    view.edge(type: edgeType) == view.superview!.edge(type: edgeType)
+                }
+            }
+        }
+        
+        constrain(self.first!) { view in
+            view.start(dir) == view.superview!.start(dir)
+        }
+        
+        for i in 1..<self.count {
+            constrain(self[i], self[i-1]) { current, previous in
+                current.start(dir) == previous.end(dir) + margin
+            }
+        }
+        
+        constrain(self.last!) { view in
+            view.end(dir) == view.superview!.end(dir) ~ 250
+        }
+        
+        return container
+    }
+    
+    public func equalWidths() {
+        for i in 1...(self.count - 1) {
+            constrain(self[i], self[i - 1]) { current, previous in
+                current.width == previous.width
+            }
+        }
+    }
+    
+    public func equalHeights() {
+        for i in 1...(self.count - 1) {
+            constrain(self[i], self[i - 1]) { current, previous in
+                current.height == previous.height
+            }
+        }
+    }
+}
+
+public extension NSView {
+    @discardableResult public func append(_ view: NSView, left: CGFloat = 0, top: CGFloat = 0) -> NSView {
+        addSubview(view)
+        
+        constrain(view) { view in
+            view.left == view.superview!.left + left
+            view.top == view.superview!.top + top
+        }
+        
+        return view
+    }
+    
+    @discardableResult public func include(_ view: NSView, inset ins: CGFloat = 0) -> NSView {
+        addSubview(view)
+        
+        constrain(view) { view in
+            view.edges == inset(view.superview!.edges, ins, ins, ins, ins)
+        }
+        
+        return view
+    }
+    
+    @discardableResult public func include(_ view: NSView, insets ins: EdgeInsets) -> NSView {
+        addSubview(view)
+        
+        constrain(view) { view in
+            view.edges == inset(view.superview!.edges, ins.top, ins.left, ins.bottom, ins.right)
+        }
+        
+        return view
+    }
+    
+    @discardableResult public func height(_ height: CGFloat) -> NSView {
+        constrain(self) { view in
+            view.height == height
+        }
+        
+        return self
+    }
+    
+    @discardableResult public func width(_ width: CGFloat) -> NSView {
+        constrain(self) { view in
+            view.width == width
+        }
+        
+        return self
+    }
+    
+    @discardableResult public func expand(_ dir: StackDirection) -> NSView {
+        constrain(self) { view in
+            view.start(dir) == view.superview!.start(dir)
+            view.end(dir) == view.superview!.end(dir)
+        }
+        
+        return self
+    }
+}
