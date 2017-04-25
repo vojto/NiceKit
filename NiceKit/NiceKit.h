bf5f549203a1f2fb8d9f4b84f7fcfef56aa37b8d
Vojtech Rinik <vojto@rinik.net>
2017-04-25 10:29:23 +0200

    Initial Commit

---
+++ NiceKit/NiceKit.h
@@ -0,0 +1,19 @@
+//
+//  NiceKit.h
+//  NiceKit
+//
+//  Created by Vojtech Rinik on 4/25/17.
+//  Copyright © 2017 Median. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NiceKit.
+FOUNDATION_EXPORT double NiceKitVersionNumber;
+
+//! Project version string for NiceKit.
+FOUNDATION_EXPORT const unsigned char NiceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NiceKit/PublicHeader.h>
+
+
